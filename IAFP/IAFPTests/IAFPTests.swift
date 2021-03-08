b63ba544fe75caa74d5559239e8b3e37a9ea280e
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-03-08 13:21:55 +0330

    Add files via upload

---
+++ IAFP/IAFPTests/IAFPTests.swift
@@ -0,0 +1,34 @@
+//
+//  IAFPTests.swift
+//  IAFPTests
+//
+//  Created by ardavan on 8/15/1398 AP.
+//  Copyright © 1398 Ardavan. All rights reserved.
+//
+
+import XCTest
+@testable import IAFP
+
+class IAFPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
